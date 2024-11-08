3a0bf557f30b7a67b8b9dcb333bcde2aeb321c38
Peter-DevX <peterasiedugyan0@gmail.com>
2024-11-08 12:46:20 +0000

    implementation of JavaScript
    Contact
    Implementation of form with google docs and JavaScript.

--- portfolio website/app.js
+++ portfolio website/app.js
@@ -11,3 +11,30 @@ function opentab(tabname) {
     document.getElementById(tabname).classList.add('active-tab');
     event.currentTarget.classList.add("active-link");
 }
+
+var sidemenu = document.getElementById('sidemenu')
+
+function openmenu(){
+    sidemenu.style.right = '0';
+}
+function closemenu(){
+    sidemenu.style.right = '-200px'
+}
+
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbyfCxAmL32X4YNRgOisRDyl9e0Trc3q03M1rjquR2veiOPGinLHS7bkkjWxgi_8QeH_2A/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById('msg')
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message sent successfully"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },3000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
